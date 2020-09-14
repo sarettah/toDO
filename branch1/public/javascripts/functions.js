ecb6d89572626b0b90a8129d45373bea0c4713e9
sarettah <70282491+sarettah@users.noreply.github.com>
2020-09-14 18:16:47 +0200

    update nota

--- branch1/public/javascripts/functions.js
+++ branch1/public/javascripts/functions.js
@@ -1,4 +1,6 @@
 const path = require('path');
+var fs = require('fs');
+const {ObjectId} = require('mongodb'); 
 
  async function addNewUser(nome, email, password, db, res) {
    if(nome !==null && nome !== "" && email !==null && email !== "" && password !==null && password !== ""){
@@ -59,6 +61,81 @@ async function findUser( email, password, db, res) {
       res.send("email o password vuoti")
    }
 }
+function findListaNote(db, res){
+   try {          
+     console.log("cerco lista note in db dell'utente con id "+currentUser.id.toString());
+     const query = {  idUser: currentUser.id.toString() };
+     //const projection = { _id: 1, nome: 1 };
+     db.collection('note').find(query )
+     .toArray(function(err, result) {
+       if (err) throw err;
+       console.log("result: "+result);
+       var note = [] = result;
+       fs.readFile(path.join('c:/Users/Consoft/Desktop/Progetti Personali Sarah/toDO/branch1/public/lista.html'), function(err, data) {
+         if (err) throw err;
+         res.write(data);
+         if(note !== null && note.length >0){
+           
+           for(var i = 0; i<note.length; i++){
+             //var nota = result[i];
+             var check = "";
+             if(result[i].checked){
+               check='<input type="checkbox" id="'+result[i]._id.toString()+'" onclick="check(\' '+result[i]._id.toString()+' \', \'true\' )" checked>'
+             }else{
+               check='<input type="checkbox" id="'+result[i]._id.toString()+'" onclick="check(\' '+result[i]._id.toString()+' \', \'false\')" >'
+             }
+             res.write('<p>'+
+             '<br> Titolo:      '+result[i].titolo+
+             '<br> descrizione: '+result[i].note+
+             '<br> fatto:'+check+
+             '<br>'+
+             '<button><a href="modifica?id='+result[i]._id.toString()+'&titolo='+result[i].titolo+'&descrizione='+result[i].note+'" >modifica</a></button>'+
+             '</p>');
+           }
+           res.end();
+   
+         }else{
+           res.end("non hai ancora note salvate");
+         }
+         
+        });
+ 
+     });
+   } catch (e) {
+       console.error(e);
+   } 
+ }
+ function addNewNota(titolo, descrizione,db, res){
+   if(titolo === null || titolo === "")
+     res.end("bisogna inserire il titolo");
+   db.collection('note').insertOne({
+       titolo: titolo,
+       idUser: currentUser.id,
+       note: descrizione,
+       checked: false
+     }).then(function(){
+       res.redirect('/lista');
+     });
+ }
+
+ function updateNota(id,titolo,descrizione,db, res){
+  // console.log("ischecked "+ischecked);
+  if(titolo === null || titolo === ""){
+     res.end("il titolo deve essere valorizzato!");
+  }else{
+     db.collection('note').updateOne(
+     { _id:ObjectId(id.trim()) },
+     {
+       $set: { 'titolo': titolo, 'note':descrizione },
+       $currentDate: { lastModified: true }
+     }).then(function(){
+      res.redirect('/lista');
+      });
+  }
+}
 
 module.exports.addNewUser = addNewUser;
 module.exports.findUser = findUser;
+module.exports.findListaNote = findListaNote;
+module.exports.addNewNota = addNewNota;
+module.exports.updateNota = updateNota;
--- branch1/server.js
+++ branch1/server.js
@@ -44,6 +44,52 @@ router.get('/',function(req,res){
    res.sendFile(path.join(__dirname+'/public/login.html'));
    //__dirname : It will resolve to your project folder.
  });
+////////////////////////////////////////////////////////////home/////////////////////////////////////////////////////////
+router.get('/home',function(req,res){
+  res.sendFile(path.join(__dirname+'/public/home.html'));
+  //__dirname : It will resolve to your project folder.
+});
+
+////////////////////////////////////////////////////////modifica ed elimina//////////////////////////////////////////////////
+router.get('/modifica',function(req,res){
+  var id = req.query.id;
+  var titolo = req.query.titolo;
+  var descrizione = req.query.descrizione;
+  //console.log("modifica! "+id+" "+titolo+" "+descrizione);
+  res.write('<script src="/javascripts/nav.js"></script>'+
+  '<h3>modifica</h3>'+
+  '<form action="/updateNota" method="POST">'+
+  '<input type="hidden" id="id" name="id" value="'+id+'"'+
+  '<label for="titolo">Titolo:</label>'+
+  '<input type="text" id="titolo" name="titolo" value="'+titolo+'"><br><br>'+
+  '<label for="descrizione">Descrizione:</label>'+
+  '<input type="text" id="descrizione" name="descrizione" value="'+descrizione+'"><br><br>'+
+  '<input type="submit" value="Salva">'+
+  '</form>'
+  );
+res.end();
+  //res.sendFile(path.join(__dirname+'/public/modifica.html'));
+  //res.write("ok");
+});
+router.post('/updateNota', function(req, res){
+  var body="";
+  req.on('data', chunk => {
+    body += chunk.toString(); // convert Buffer to string
+  });
+  req.on('end', () => {
+    //console.log("body  "+body);
+    var json = parse(body)
+    console.log("json: "+JSON.stringify(json))
+    titolo = json.titolo;
+    descrizione = json.descrizione;
+    id=json.id;
+    console.log("json  "+titolo + " + "+descrizione)
+    functions.updateNota(id,titolo,descrizione,db,res);
+    
+  }); 
+  //res.sendFile(path.join(__dirname+'/public/nuovo.html'));
+ 
+});
 
 
 ///////////////////////////////////////////////////////////////nuova nota//////////////////////////////////////////////////
@@ -63,74 +109,17 @@ router.post('/newNota', function(req, res){
     titolo = json.titolo;
     descrizione = json.descrizione;
     console.log("json  "+titolo + " + "+descrizione)
-    addNewNota(titolo, descrizione, res);
+    functions.addNewNota(titolo, descrizione,db, res);
   }); 
   //res.sendFile(path.join(__dirname+'/public/nuovo.html'));
  
 });
-function addNewNota(titolo, descrizione, res){
-  if(titolo === null || titolo === "")
-    res.end("bisogna inserire il titolo");
-  db.collection('note').insertOne({
-      titolo: titolo,
-      idUser: currentUser.id,
-      note: descrizione,
-      checked: false
-    }).then(function(){
-      res.redirect('/lista');
-    });
-  
-  
-}
-
 /////////////////////////////////////////////////////////////per la lista////////////////////////////////////////////////
 
 router.get('/lista', (req, res) =>{
-  findListaNote(res);    
+  functions.findListaNote(db,res);    
 });
 
-const findListaNote =(res) =>{
-  try {          
-    console.log("cerco lista note in db dell'utente con id "+currentUser.id.toString());
-    const query = {  idUser: currentUser.id.toString() };
-    //const projection = { _id: 1, nome: 1 };
-    db.collection('note').find(query )
-    .toArray(function(err, result) {
-      if (err) throw err;
-      console.log("result: "+result);
-      var note = [] = result;
-      fs.readFile(path.join(__dirname+'/public/lista.html'), function(err, data) {
-        if (err) throw err;
-        res.write(data);
-        if(note !== null && note.length >0){
-          
-          for(var i = 0; i<note.length; i++){
-            //var nota = result[i];
-            var check = "";
-            if(result[i].checked){
-              check='<input type="checkbox" id="'+result[i]._id.toString()+'" onclick="check(\' '+result[i]._id.toString()+' \', \'true\' )" checked>'
-            }else{
-              check='<input type="checkbox" id="'+result[i]._id.toString()+'" onclick="check(\' '+result[i]._id.toString()+' \', \'false\')" >'
-            }
-            res.write('<p>'+
-            '<br> Titolo:      '+result[i].titolo+
-            '<br> descrizione: '+result[i].note+
-            '<br> fatto:'+check+
-            '</p>');
-          }
-          res.end();
-  
-        }else{
-          res.end("non hai ancora note salvate");
-        }
-        
-       });
-
-    });
-  } catch (e) {
-      console.error(e);
-  } 
-}
 /////////////////////////////////////////////////quando clicco una checkbox//////////////////////////////////////////
 router.post('/check', function(req, res){
   
@@ -142,7 +131,7 @@ router.post('/check', function(req, res){
       //console.log("body  "+body);
       var json = JSON.parse(body)
       console.log("body  "+json.id+ " "+json.ischecked);
-      if(json.ischecked === true)
+      if(json.ischecked === true || json.ischecked === 'true'  )
         modifyNoteCheck(json.id, false, res)
       else
         modifyNoteCheck(json.id, true, res)
@@ -152,7 +141,7 @@ router.post('/check', function(req, res){
     //modifyNoteCheck(res);
 });
 function modifyNoteCheck(idNota,ischecked, res){
-  console.log("ischecked"+ischecked);
+  console.log("ischecked "+ischecked);
    db.collection('note').updateOne(
     { _id:ObjectId(idNota.trim()) },
     {
@@ -162,8 +151,7 @@ function modifyNoteCheck(idNota,ischecked, res){
   ).then(function(){
       res.end("ok");
       
-  });
-  
+  }); 
 }
 
  ///////////////////////////////////////////////////////////per il login/////////////////////////////////////////////////////
@@ -184,56 +172,14 @@ function modifyNoteCheck(idNota,ischecked, res){
      console.log("json  "+email + " + "+password)
      functions.findUser(email, password,db, res);
     });  
- });/*
- async function findUser( email, password, res) {
-   if(email !==null && email !== "" && password !==null && password !== ""){
-         console.log("campi validi")
-         try {          
-             console.log("cerco user in db");
-             const query = {  email: email, password: password };
-             const projection = { _id: 1, nome: 1 };
-             db.collection('users').find(query, { projection: projection } )
-             .toArray(function(err, result) {
-               if (err) throw err;
-               //console.log(result[0])
-               if(typeof result[0] === 'undefined' ){
-                  res.end("non ho trovato l'utente")
-                  console.log("non ho trovato l'utente")
-               }else{
-                  var id = result[0]._id;
-                  var nome = result[0].nome//result.projection.nome;
-                  //console.log("result: nome "+nome+ " + id "+id);
-                  currentUser = {id: id.toString(), nome:nome, email:email, password:password};
-                 console.log("currentUser: "+ JSON.stringify(currentUser));
-                  /*fs.readFile(path.join(__dirname+'/public/home.html'), function(err, data) {
-                    if (err) throw err;
-                    //res.write('<p>Ciao '+nome+'</p>');
-                    res.write(data);
-                    res.end();
-                   });*/
-                  /*res.sendFile(path.join(__dirname+'/public/home.html'));
-                            
-               }
-              
-            })
-            
-     
-         } catch (e) {
-             console.error(e);
-         } 
-   }else{
-      res.send("email o password vuoti")
-   }
-}
-*/
+ });
 
 
 
  ///////////////////////////////////////////////////////per la registrazione////////////////////////////////////////////////
 router.get('/registrati', (req, res) => {
    res.sendFile(path.join(__dirname+'/public/register.html'));
-   console.log("click registrati server side");
-  
+   //console.log("click registrati server side");
  });
 
  router.post('/newUser', (req, res) => {
@@ -261,34 +207,7 @@ router.get('/registrati', (req, res) => {
    });
   
  });
- /*async function addNewUser(nome, email, password, res) {
-   if(nome !==null && nome !== "" && email !==null && email !== "" && password !==null && password !== ""){
-         console.log("campi validi")
-         try {          
-             console.log("aggiungo dati in db");
-             db.collection('users').insertOne({
-               nome: nome,
-               email: email,
-               password: password
-             })
-             .then(function(result) {
-               // process result
-               console.log("query insert andata a buon fine");
-               //console.log("chiudo connessione")
-               //client.close();
-               res.sendFile(path.join(__dirname+'/public/home.html'));
-             }).catch(function(err){
-               console.log("errore: "+err)
-             })
-     
-         } catch (e) {
-             console.error(e);
-         } 
-   }else{
-      res.send("uno o più campi vuoti vuoti")
-   }
-}*/
-
+ 
 
 
 
